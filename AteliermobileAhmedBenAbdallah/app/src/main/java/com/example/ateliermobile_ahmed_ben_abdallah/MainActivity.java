d736798f8b7ba5126432db2fee4ac3c8d3a454d7
Ahmed Ben Abdallah <63337471+Ahmedbenabdallah29435@users.noreply.github.com>
2023-09-28 14:22:49 +0200

    trying 3 type of layout

---
+++ AteliermobileAhmedBenAbdallah/app/src/main/java/com/example/ateliermobile_ahmed_ben_abdallah/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ateliermobile_ahmed_ben_abdallah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constrait_layout);
+    }
+}
---
+++ AteliermobileAhmedBenAbdallah/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Ateliermobile-Ahmed-Ben-Abdallah"
+include(":app")
+ 
